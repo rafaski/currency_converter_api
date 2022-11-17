8a0ad5ca7256ef7d063abe7ecd0dd438278b6e3d
Rafal <99764836+rafaski1@users.noreply.github.com>
2022-11-17 19:30:10 +0100

    Refactor endpoints to build request params as a dict; add separate /historical endpoint

--- currency_converter_api/routers/converter.py
+++ currency_converter_api/routers/converter.py
@@ -18,9 +18,16 @@ async def currencies(request: Request):
     """
     Fetch a list of supported currencies
     """
-    currencies_url = f"currencies?api_key={API_KEY}"
-    response = requests.get(BASE_URL + currencies_url, headers=HEADERS)
-    return Output(success=True, results=[response.text])
+    endpoint = "currencies"
+    params = {
+        "api_key": API_KEY
+    }
+    response = requests.get(
+        url=f"{BASE_URL}{endpoint}",
+        params=params,
+        headers=HEADERS
+    )
+    return Output(success=True, results=response.json())
 
 
 @router.get("/convert", response_model=Output)
@@ -28,26 +35,57 @@ async def convert(
     request: Request,
     amount: int,
     from_curr: str,
-    to_curr: str,
-    date: Optional[str] = None
+    to_curr: str
 ):
     """
     Convert an amount of one currency into another currency.
     from_curr : Base currency symbol
     to_curr : Target currency symbol
     amount : Amount of source currency to convert
-    date: UTC date in YYYY-MM-DD format. Must be within the last 14 days.
     """
-    convert_url = f"convert?from={from_curr}&to={to_curr}&amount={amount}&" \
-                  f"api_key={API_KEY}"
-    response = requests.get(BASE_URL + convert_url, headers=HEADERS)
-    if date is not None:
-        historical_url = f"historical?date={date}&from={from_curr}&" \
-                         f"to={to_curr}&api_key={API_KEY}"
-        response2 = requests.get(BASE_URL + historical_url, headers=HEADERS)
-        return Output(success=True, results=[response.text, response2.text])
-    else:
-        return Output(success=True, results=[response.text])
+    endpoint = "convert"
+    params = {
+        "from": from_curr,
+        "to": to_curr,
+        "amount": amount,
+        "api_key": API_KEY
+    }
+    response = requests.get(
+        url=f"{BASE_URL}{endpoint}",
+        params=params,
+        headers=HEADERS
+    )
+    return Output(success=True, results=response.json())
+
+
+@router.get("/historical", response_model=Output)
+async def convert(
+        request: Request,
+        amount: int,
+        from_curr: str,
+        to_curr: str,
+        date: Optional[str] = None
+):
+    """
+    Gets you historical conversion data. Due to trial version,
+    date must be limited to within tle last 14 days
+    from_curr : Base currency symbol
+    to_curr : Target currency symbol
+    date: UTC date in YYYY-MM-DD format
+    """
+    endpoint = "historical"
+    params = {
+        "date": date,
+        "from": from_curr,
+        "to": to_curr,
+        "api_key": API_KEY
+    }
+    response = requests.get(
+        url=f"{BASE_URL}{endpoint}",
+        params=params,
+        headers=HEADERS
+    )
+    return Output(success=True, results=response.json())
 
 
 @router.get("/fetch_one", response_model=Output)
@@ -61,9 +99,18 @@ async def fetch_one(
     from_curr : Base currency symbol
     to_curr : Target currency symbol
     """
-    fetch_one_url = f"fetch-one?from={from_curr}&to={to_curr}&api_key={API_KEY}"
-    response = requests.get(BASE_URL + fetch_one_url, headers=HEADERS)
-    return Output(success=True, results=[response.text])
+    endpoint = "fetch-one"
+    params = {
+        "from": from_curr,
+        "to": to_curr,
+        "api_key": API_KEY
+    }
+    response = requests.get(
+        url=f"{BASE_URL}{endpoint}",
+        params=params,
+        headers=HEADERS
+    )
+    return Output(success=True, results=response.json())
 
 
 @router.get("/fetch_all", response_model=Output)
@@ -72,6 +119,14 @@ async def fetch_all(request: Request, from_curr: str):
     Fetch all available currency rates.
     from_curr : Base currency symbol
     """
-    fetch_all_url = f"fetch-all?from={from_curr}&api_key={API_KEY}"
-    response = requests.get(BASE_URL + fetch_all_url, headers=HEADERS)
-    return Output(success=True, results=[response.text])
+    endpoint = "fetch-all"
+    params = {
+        "from": from_curr,
+        "api_key": API_KEY
+    }
+    response = requests.get(
+        url=f"{BASE_URL}{endpoint}",
+        params=params,
+        headers=HEADERS
+    )
+    return Output(success=True, results=response.json())
