b5832e9bbb0f3f7fa47b420ee7567a1f902aceb3
Rafal <99764836+rafaski1@users.noreply.github.com>
2022-11-17 17:35:02 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Request
+from typing import Optional
+import requests
+
+from schemas import Output
+from _secrets import key
+
+app = FastAPI()
+
+API_KEY = key
+BASE_URL = "https://api.fastforex.io/"
+HEADERS = {"accept": "application/json"}
+
+
+@app.get("/currencies", response_model=Output)
+async def currencies(request: Request):
+    """
+    Fetch a list of supported currencies
+    """
+    currencies_url = f"currencies?api_key={API_KEY}"
+    response = requests.get(BASE_URL + currencies_url, headers=HEADERS)
+    return Output(success=True, results=[response.text])
+
+
+@app.get("/convert", response_model=Output)
+async def convert(
+    request: Request,
+    amount: int,
+    from_curr: str,
+    to_curr: str,
+    date: Optional[str] = None
+):
+    """
+    Convert an amount of one currency into another currency.
+    from_curr : Base currency symbol
+    to_curr : Target currency symbol
+    amount : Amount of source currency to convert
+    date: UTC date in YYYY-MM-DD format. Must be within the last 14 days.
+    """
+    convert_url = f"convert?from={from_curr}&to={to_curr}&amount={amount}&" \
+                  f"api_key={API_KEY}"
+    response = requests.get(BASE_URL + convert_url, headers=HEADERS)
+    if date is not None:
+        historical_url = f"historical?date={date}&from={from_curr}&" \
+                         f"to={to_curr}&api_key={API_KEY}"
+        response2 = requests.get(BASE_URL + historical_url, headers=HEADERS)
+        return Output(success=True, results=[response.text, response2.text])
+    else:
+        return Output(success=True, results=[response.text])
+
+
+@app.get("/fetch_one", response_model=Output)
+async def fetch_one(
+    request: Request,
+    from_curr: str,
+    to_curr: str
+):
+    """
+    Fetch a single currency exchange rate, from and to any supported currency.
+    from_curr : Base currency symbol
+    to_curr : Target currency symbol
+    """
+    fetch_one_url = f"fetch-one?from={from_curr}&to={to_curr}&api_key={API_KEY}"
+    response = requests.get(BASE_URL + fetch_one_url, headers=HEADERS)
+    return Output(success=True, results=[response.text])
+
+
+@app.get("/fetch_all", response_model=Output)
+async def fetch_all(request: Request, from_curr: str):
+    """
+    Fetch all available currency rates.
+    from_curr : Base currency symbol
+    """
+    fetch_all_url = f"fetch-all?from={from_curr}&api_key={API_KEY}"
+    response = requests.get(BASE_URL + fetch_all_url, headers=HEADERS)
+    return Output(success=True, results=[response.text])
