33c32552fe80f579d8b8edaf8a6510a18b9e021e
Rafal <99764836+rafaski1@users.noreply.github.com>
2022-11-17 17:53:28 +0100

    Add FastAPI app title, description and contact metadata

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ import requests
 from schemas import Output
 from _secrets import key
 
-app = FastAPI()
+description = """
+Currency Converter allows you to:
+
+* **get all supported currencies** 
+* **convert currencies** 
+* **fetch a single currency exchange rate** 
+* **fetch all currency exchange rate** 
+
+Based on fastforex.io api.
+"""
+
+app = FastAPI(
+    title="Currency Converter",
+    description=description,
+    contact={
+        "name": "FastForex",
+        "url": "https://www.fastforex.io/",
+    },
+    )
 
 # apikey provided upon request
 API_KEY = key
