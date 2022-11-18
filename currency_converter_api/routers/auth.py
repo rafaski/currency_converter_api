4b188bce8a08daa9e721096a63e1bba420cab2bf
Rafal <99764836+rafaski1@users.noreply.github.com>
2022-11-18 21:21:54 +0100

    Add verify_user auth dependency

--- currency_converter_api/routers/auth.py
+++ currency_converter_api/routers/auth.py
@@ -8,3 +8,12 @@ async def verify_token(request: Request):
     if headers.get("x-token") == X_TOKEN:
         return headers.get("x-token")
     raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_user(request: Request, login: str, password: str):
+    headers = dict(request.headers)
+    if headers.get("login") == login and headers.get("password") == password:
+        return headers.get("login"), headers.get("password")
+    raise HTTPException(status_code=400, detail="User unauthorized")
+
+
