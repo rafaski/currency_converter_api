a9bda168bd8c7c075a981a3b5473710f7fec0eea
rafaski1 <rav.skibinski@gmail.com>
2022-12-07 12:48:59 +0100

    sql

--- currency_converter_api/main.py
+++ currency_converter_api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+
 from routers.converter import router as converter_router
 from routers.users import router as user_router
+from sql.database import database
+
 
 description = """
 Currency Converter allows you to:
@@ -24,3 +27,13 @@ app = FastAPI(
 
 app.include_router(user_router)
 app.include_router(converter_router)
+
+
+@app.on_event("startup")
+async def startup():
+    database.create_session()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    database.dispose_session()
--- currency_converter_api/sql/database.py
+++ currency_converter_api/sql/database.py
@@ -1,11 +1,50 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from typing import Optional
+from functools import wraps
 
 DATABASE_URL = "sqlite:///./sql_app.db"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def database_operation(func):
+    @wraps(func)
+    async def _database_operation(*args, **kwargs):
+        open_connection = database.session()
+        try:
+            return await func(open_connection, *args, **kwargs)
+        except Exception as e:
+            print(e)
+            open_connection.rollback()
+        finally:
+            open_connection.close()
+    return _database_operation
+
+
+class Database:
+    session: Optional[sessionmaker] = None
+    engine = None
+
+    def __init__(self, database_url: str):
+        self.database_url = database_url
+
+    def create_session(self):
+        self.engine = create_engine(
+            DATABASE_URL, connect_args={"check_same_thread": False}
+        )
+        self.session = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=self.engine
+        )
+
+    def dispose_session(self):
+        self.session.close_all()
+        self.engine.dispose()
+        self.session = None
+
+
+database = Database(database_url=DATABASE_URL)
+
